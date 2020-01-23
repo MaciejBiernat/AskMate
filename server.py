fa0234ffc383a1c6fe275c049b72d1f7f9b0894f
Anna Barczyk <anna.ewa.barczyk@gmail.com>
2020-01-23 10:37:41 +0100

    Added list template

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def route_list():
-    data_manager.time_decoding()
+    data_manager.time_decoding("question.csv")
     return render_template("index.html")
 
 

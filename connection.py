1ceb7d75e497e2ed0c7ddce80a69ead124e140f8
Anna Barczyk <anna.ewa.barczyk@gmail.com>
2020-02-04 11:18:06 +0100

    added changes to connect with database

--- connection.py
+++ connection.py
@@ -1,16 +1,68 @@
-import csv
+# import csv
+# import os
+#
+# #DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
+# # Dodać obsługę błędów, co jak nie będzie pliku?
+#
+# def reader_csv(filename):
+#     dictionaries = csv.DictReader(open(filename))
+#     return dictionaries
+#
+# def writer_csv(filename, fieldnames, new_data):
+#     with open(filename, 'a') as my_file:
+#         writer = csv.DictWriter(my_file, fieldnames=fieldnames)
+#         writer.writerow(new_data)
+
+# Creates a decorator to handle the database connection/cursor opening/closing.
+# Creates the cursor with RealDictCursor, thus it returns real dictionaries, where the column names are the keys.
 import os
+import psycopg2
+import psycopg2.extras
 
-#DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
+
+def get_connection_string():
+    # setup connection string
+    # to do this, please define these environment variables first
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
+
+    env_variables_defined = user_name and password and host and database_name
+
+    if env_variables_defined:
+        # this string describes all info for psycopg2 to connect to the database
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
 
-def reader_csv(filename):
-    dictionaries = csv.DictReader(open(filename))
-    return dictionaries
+def open_database():
+    try:
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
+        raise exception
+    return connection
 
-def writer_csv(filename, fieldnames, new_data):
-    with open(filename, 'a') as my_file:
-        writer = csv.DictWriter(my_file, fieldnames=fieldnames)
-        writer.writerow(new_data)
+
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
+
+    return wrapper
 
 
--- server.py
+++ server.py
@@ -13,84 +13,21 @@ def route_list():
 
 
 @app.route('/question')
-def list_questions():
-    '''Implement the /list page that displays all questions.
 
-    The page is available under /list
-    Load and display the data from question.csv
-    Sort the questions by the latest question on top
-    '''
-
-    titles = ['ID', 'Submission Time', 'View Number', 'Vote Number', 'Title']
-    all_questions = data_manager.time_decoding('question.csv')
-    for question in all_questions:
-        print(question)
-    return render_template("list.html", all_questions=all_questions, titles=titles)
 
 
 @app.route('/question/<int:question_id>')
-def display_question(question_id):
-    '''Create the /question/<question_id> page that displays a question and the answers for it.
 
-    The page is available under /question/<question_id>
-    There are links to the question pages from the list page
-    The page displays the question title and message
-    The page displays all the answers to a question'''
-    all_questions = data_manager.time_decoding('question.csv')
-    all_answers = data_manager.time_decoding('answer.csv')
 
-    for row in all_questions:
-        if row['id'] == str(question_id):
-            question = row
-    answers = []
-    for row in all_answers:
-        if row['id'] == str(question_id):
-            answers.append(row)
-
-    return render_template('question.html', question=question, answers=answers)
 
 @app.route('/add-question', methods=["GET", "POST"])
 def ask_question():
-    '''
-    Implement a form that allows you to add a question.
-
-    There is an /add-question page with a form
-    The page is linked from the list page
-    There is a POST form with at least title and message fields
-    After submitting, you are redirected to "Display a question" page of this new question
-    :return:
-    '''
-    new_question = {}
-    titles = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
-    if request.method == "POST":
-        question_id = data_manager.generate_next_id("question.csv")
-        new_question["id"] = question_id
-        new_question["submission_time"] = "1493068124"
-        new_question["view_number"] = "0"
-        new_question["vote_number"] = "0"
-        new_question["title"] = request.form["title"]
-        new_question["message"] = request.form["message"]
-        new_question["image"] = "img"
-        # dodać zapisywanie z potrzebnymi innymi kolumnami
-        # ewentualnie dodać jakoś ten image
-        connection.writer_csv("question.csv", titles, new_question)
-        return redirect(f'/question/{question_id}')
-    return render_template("add-question.html")
 
 
 @app.route('/question/<question_id>/new-answer')
 def post_an_answer():
-    '''
-    Implement posting a new answer.
 
-    The page URL is /question/<question_id>/new-answer
-    The question detail page links to this page
-    The page has a POST form with a form field called message
-    Posting an answer redirects you back to the question detail page, and the new answer is there
-    :return:
-    '''
 
-    return render_template('question.html')
 
 if __name__ == '__main__':
     app.run(
